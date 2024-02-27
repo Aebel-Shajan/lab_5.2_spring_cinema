2f09a4019078ea50328b8ff812c744837a178c16
Aebel Shajan <aebelshajan@gmail.com>
2024-02-27 15:57:47 +0000

    Create MovieRepository to connect model to database

---
+++ src/main/java/com/example/spring_cinema/repositories/MovieRepository.java
@@ -0,0 +1,10 @@
+package com.example.spring_cinema.repositories;
+
+import com.example.spring_cinema.models.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+}
